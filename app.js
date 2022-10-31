47f4e1d17d520b80f242c21db68c538f16dd5c5b
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-10-31 23:18:58 +0300

    Hw #12

--- app.js
+++ app.js
@@ -2,23 +2,32 @@
 function fromNumberToString(number, base) {
     number = Math.abs(number);
     let res = "";
-    do {
-        let digit = number % base;
-        res += digit.toString(base);
-        number = Math.trunc(number / base);
-    } while (number != 0);
+    if (base > 36 || base < 2) {
+        console.log("ERROR");
+    }
+    else {
+        do {
+            let digit = number % base;
+            res += digit.toString(base);
+            number = Math.trunc(number / base);
+        } while (number != 0);
+
+    }
+
 
     return res.split("").reverse().join("");
 }
 console.log(fromNumberToString(900550, 36));
 console.log(fromNumberToString(46016237, 36));
 console.log(fromNumberToString(11483, 2));
+console.log(fromNumberToString(123, 40));
 
 //************************************************************************ */
 
 function fromStringToNumber(string, base) {
 
     let result = parseInt(string, base);
+
     if (isNaN(result)) {
         return String.fromCharCode(base);
     }
@@ -26,6 +35,7 @@ function fromStringToNumber(string, base) {
         return result;
     }
 }
+
 console.log(fromStringToNumber("JAVA", 36));
 console.log(fromStringToNumber("react’", 36));
-console.log(fromStringToNumber("10110011011011’", 2));
+console.log(fromStringToNumber("10110011011011", 2));
