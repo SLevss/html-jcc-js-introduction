8415f55e421a50367b61d67dfa5b2f01bb509102
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-02 08:35:51 +0300

    HW #12

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function fromNumberToString(number, base) {
             if (digit > 9) {
                 digit = String.fromCharCode(55 + digit);
             }
-            
+
             res = digit + res;
             number = Math.trunc(number / base);
         } while (number != 0);
@@ -30,24 +30,42 @@ console.log(fromNumberToString(11483, 2));
 
 function fromStringToNumber(string, base) {
 
-
+    if (isNaN(base)) {
+        console.log("ERROR");
+    }
+    if (base > Math.trunc(base)) {
+        console.log("ERROR");
+    }
     if (base > 36 || base < 2) {
         console.log("ERROR");
     }
-    else if (typeof string == "number") {
+    if (typeof string != 'string') {
         console.log("ERROR");
-    }
-    else {
-        res = parseInt(string, base);
-        if (isNaN(res)) {
-            return String.fromCharCode(base);
-        }
-        else {
-            return res;
-        }
-    }
-}
+    }    
+        let result = 0;
+        // string.toLowerCase();
+        for (let i = 0; i < string.length; i++) {
 
-console.log(fromStringToNumber("JAVA", 36));
-console.log(fromStringToNumber("react’", 36));
-console.log(fromStringToNumber("10110011011011", 2));
+            let digit = string.charCodeAt(i);
+            if (digit >= 48 && digit <= 57) {
+
+                digit -= 48;
+            } else if (digit >= 97 && digit <= 122) {
+
+                digit -= 87;
+            } else {
+                return 'error';
+            }
+
+            result = result * base + digit;
+        }
+        return result;
+    }
+
+
+
+    console.log(fromStringToNumber('java', 36));  // => 900550
+    console.log(fromStringToNumber('react', 36));  // => 46016237
+    console.log(fromStringToNumber('10110011011011', 2));  // => 11483
+    
+    //---------------isBaseValid------------------------------
