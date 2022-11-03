8dae48dffac98e735b7afe7fe2366d683684bc94
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-03 22:19:56 +0300

    HW #13

--- validID.js
+++ validID.js
@@ -1,20 +1,16 @@
 //-------------------------check Teudat Zehut----------------------------------
 function checkTeudatZehut(TeudatZehut) {
 
-    if (TeudatZehut.length != 9) {
-        return false;
-    }
-    if (isNaN(TeudatZehut)) {
-        return false;
-    }
-
+    if (TeudatZehut.length != 9 || isNaN(TeudatZehut)) return false;
+    
     let arTeudatNumber = inArray(TeudatZehut);
-
+   
     let newarTeudatNumber = arTeudatNumber.map((digit, index) => {
         if (index % 2 !== 0) return sumDigits(digit * 2);
         else return sumDigits(digit);
-    });
 
+    });
+   
     let res = 0;
     const controlSumm = newarTeudatNumber.reduce((partSum, x) => partSum + x, 0);
     console.log("Checksum =", controlSumm);
@@ -27,19 +23,22 @@ function checkTeudatZehut(TeudatZehut) {
 }
 console.log(checkTeudatZehut('123456782'));
 console.log(checkTeudatZehut('346698004'));
-// F.ex.
+// F.ex
 console.log(checkTeudatZehut('123456789'));
-console.log(checkTeudatZehut('12345678995'));
-function inArray(teudatNumber) {
+console.log(checkTeudatZehut('34669800410'));
 
+function inArray(teudatNumber) {
     let arTeudatNumber = Array.from(teudatNumber);
     return arTeudatNumber;
 }
 function sumDigits(digit) {
+
+    let rem = 0;
     let sum = 0;
     while (digit != 0) {
+        rem = digit % 10;
 
-        sum = sum + digit % 10;
+        sum = sum + rem;
         digit = Math.floor(digit / 10);
 
     }
