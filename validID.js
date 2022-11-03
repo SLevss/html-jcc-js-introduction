9ce2988b5acaaf940db1fdc3e0a7a062e4d0dc93
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-03 23:23:07 +0300

    HW #13

--- validID.js
+++ validID.js
@@ -1,7 +1,8 @@
 //-------------------------check Teudat Zehut----------------------------------
 function checkTeudatZehut(TeudatZehut) {
 
-    if (TeudatZehut.length != 9 || isNaN(TeudatZehut)) return false;
+  if (TeudatZehut.length != 9 || isNaN(TeudatZehut)) return false; 
+   
     
     let arTeudatNumber = inArray(TeudatZehut);
    
@@ -12,7 +13,7 @@ function checkTeudatZehut(TeudatZehut) {
     });
    
     let res = 0;
-    const controlSumm = newarTeudatNumber.reduce((partSum, x) => partSum + x, 0);
+    let controlSumm = newarTeudatNumber.reduce((partSum, x) => partSum + x, 0);
     console.log("Checksum =", controlSumm);
     if (controlSumm % 10 == 0) {
         console.log("Teudat Zehut valid with number", TeudatZehut);
@@ -25,6 +26,7 @@ console.log(checkTeudatZehut('123456782'));
 console.log(checkTeudatZehut('346698004'));
 console.log(checkTeudatZehut('123456789'));
 console.log(checkTeudatZehut('34669800410'));
+console.log(checkTeudatZehut('123456782a'));
 
 function inArray(teudatNumber) {
     let arTeudatNumber = Array.from(teudatNumber);
