d1407c6dbbe3b787e2296f0c4763fbf6be3fa0df
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-03 21:42:27 +0300

    HW #13

--- validID.js
+++ validID.js
@@ -2,6 +2,7 @@
 function checkTeudatZehut(TeudatZehut) {
 
     if (TeudatZehut.length != 9 || isNaN(TeudatZehut)) return false;
+    
     let arTeudatNumber = inArray(TeudatZehut);
     console.log(arTeudatNumber);
     let newarTeudatNumber = arTeudatNumber.map((digit, index) => {
@@ -14,10 +15,10 @@ function checkTeudatZehut(TeudatZehut) {
     const controlSumm = newarTeudatNumber.reduce((partSum, x) => partSum + x, 0);
     console.log("Checksum =", controlSumm);
     if (controlSumm % 10 == 0) {
-        return console.log("Teudat Zehut valid with number", TeudatZehut);
+        console.log("Teudat Zehut valid with number", TeudatZehut);
     }
     if (controlSumm % 10 !== 0) {
-        return console.log("Teudat Zehut not valid with number", TeudatZehut);
+        console.log("Teudat Zehut not valid with number", TeudatZehut);
     }
 }
 console.log(checkTeudatZehut('123456782'));
@@ -28,8 +29,6 @@ function inArray(teudatNumber) {
     return arTeudatNumber;
 }
 function sumDigits(digit) {
-    if (digit < 0)
-        digit = -digit;
 
     let rem = 0;
     let sum = 0;
@@ -41,30 +40,4 @@ function sumDigits(digit) {
 
     }
     return sum;
-}
-
-//---------------------------------------- generate Random Teudat Zehut---------------------------------
-function generateRandomTeudatZehut() {
-    let teudatNumber = "", number, counter = 0;
-
-    for (let i = 0; i < 8; i++) {
-
-        let firstElem = (i < 2) ? 0 : 2;
-        let lastElem = (i > 2) ? 3 : 9;
-        number = Math.round(Math.random() * (lastElem - firstElem));
-        teudatNumber += number;
-        counter += getInc(number, i);
-    }
-
-    return teudatNumber + (10 - (counter % 10));
-}
-
-
-console.log(generateRandomTeudatZehut());
-
-function getInc(number, i) {
-
-    let inc = Number(number) * ((i % 2) + 1);
-    return (inc > 9) ? inc -= 9 : inc;
-
 }
