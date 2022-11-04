bc9a4a08c5cb6ee91ef3f34e54bd2256145fcddd
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-04 20:14:07 +0300

    HW #13

--- validID.js
+++ validID.js
@@ -5,6 +5,7 @@ function checkTeudatZehut(teudatZehut) {
         console.log("Teudat Zehut = ", teudatZehut, '= ', false);
         return false;
     }
+    
     let newarTeudatNumber = Array.from(teudatZehut).map((digit, index) => {
         return (index % 2 !== 0) ? sumDigits(digit * 2) : sumDigits(digit);
     });
@@ -13,10 +14,9 @@ function checkTeudatZehut(teudatZehut) {
     console.log("Checksum =", controlSumm);
     if (controlSumm % 10 == 0) {
         console.log("Teudat Zehut valid with number", teudatZehut);
+        return true;
     }
-    if (controlSumm % 10 !== 0) {
-        console.log("Teudat Zehut not valid with number", teudatZehut);
-    }
+   
 }
 
 
@@ -36,3 +36,29 @@ console.log(checkTeudatZehut('346698004'));
 console.log(checkTeudatZehut('123456789'));
 console.log(checkTeudatZehut('34669800410'));
 console.log(checkTeudatZehut('123456782a'));
+console.log(checkTeudatZehut(generateRandomTeudatZehut()));
+
+function generateRandomTeudatZehut() {
+    let teudatNumber = "", number, counter = 0;
+
+    for (let i = 0; i < 8; i++) {
+
+        let firstElem = (i < 2) ? 0 : 2;
+        let lastElem = (i > 2) ? 3 : 9;
+        number = Math.round(Math.random() * (lastElem - firstElem));
+        teudatNumber += number;
+        counter += getInc(number, i);
+    }
+
+    return teudatNumber + (10 - (counter % 10));
+    
+}
+    
+  
+
+function getInc(number, i) {
+
+    let inc = Number(number) * ((i % 2) + 1);
+    return (inc > 9) ? inc -= 9 : inc;
+
+}
