401d189bb2abff273b4d3031834661eb1d1187e0
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-08 05:35:19 +0300

    HW #14

---
+++ array-methods.js
@@ -0,0 +1,58 @@
+function ulSurround() {
+
+  let strings = ["abc", "lmn", "cd"];
+  let newLastElement = "</ul>";
+  let ulSurround = strings.map(function (strings) {
+    return "<li>" + strings + "</li>";
+  }).join('","');
+  let newFirstElement = "<ul>";
+  let ulSurrounding = plusElements(ulSurround);
+  return ulSurrounding;
+
+};
+
+console.log(ulSurround());
+
+function plusElements(ulSurround) {
+  let newFirstElement = "<ul>";
+  let newLastElement = "<ul>";
+  let ulSurrounding = [newFirstElement].concat(ulSurround);
+  ulSurrounding.push(newLastElement);
+  return ulSurrounding;
+}
+
+function count(strings, str) {
+  return strings.reduce((count, el) => count + (el == str), 0);
+}
+
+let strings = ["abc", "lmn", "cd", "abc", "abc"];
+let str = "abc";
+console.log(str + " element encountered in array [" + strings + "]  " + count(strings, str));
+
+str = "ab";
+console.log(str + " element encountered in array [" + strings + "]  " + count(strings, str));
+
+function arrayCopy(src, srcPos, dst, dstPos, length) {
+
+  let newMediumArray = src.slice(srcPos, srcPos + length);//4,5,6
+  let newLastArray = dst.slice((dstPos)); //50,60,70
+  dst = dst.slice(0, dstPos).concat(newMediumArray).concat(newLastArray); //
+
+
+  return dst;
+}
+
+console.log(arrayCopy([1, 2, 3, 4, 5, 6, 7], 3, [10, 20, 30, 40, 50, 60, 70], 4, 3));
+
+
+function move(array, index, offset) {
+  // let newArray = array.slice(0,index,offset);
+  // return newArray;
+  let element = array[index];
+  array.splice(index, 1);
+  array.splice(index+offset, 0, element);
+  return array;
+}
+
+console.log(move([1, 2, 3, 4, 5, 6, 7], 3, -1));
+console.log(move([1, 2, 3, 4, 5, 6, 7], 2, 4));
