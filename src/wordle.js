6ff55a83280a9388c2098d06bac770307daa2a1e
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-18 18:02:30 +0300

    HW #17

--- src/wordle.js
+++ src/wordle.js
@@ -1,48 +1,80 @@
-const word = "table"
-const N_LETTERS = 5;
+const words = [["Types of JavaScript Operators", "arithmetic"], ["JavaScript Data Types", "strings"],
+["JavaScript Objects", "methods"], ["JavaScript - HTML DOM Methods", "innerHTML "]];
+
+let wordField;
+let seachWordsArray = [];
+let seachWordsArrayLength = 0;
+let index = 0;
+
+const sectionElement = document.querySelector(".word-guess");
+sectionElement.innerHTML = getDivsElements();
 const letterElements = document.querySelectorAll(".letter-guess");
-let guesses = document.querySelector('.guesses');
-let lastResult = document.querySelector('.lastResult');
-let guessCount = 6;
+const trialsElement = document.querySelector(".guess-trials");
+const wordTrElement = document.querySelector(".words-trials");
+const gameOverElement = document.querySelector(".game-over-message");
+const MovElement = document.querySelector(".guess-moving");
 
+let flGameOver = false;
+let trials = 0;
 
+function getDivsElements() {
+    index = Math.floor(Math.random() * words.length);
+    console.log('word - ', words[index][1]);
+    wordField = words[index][1];
+    seachWordsArray = Array.from(wordField);
+    let res = seachWordsArray.map(letter => `<div class="letter-guess">${letter}</div>`);
+    return res.join('');
+}
+
+function showTrialsMessage(trials) {
+    trialsElement.innerHTML = `remained ${trials} guess trials`;
+}
+function startGame() {
+    if (flGameOver) {
+        sectionElement.innerHTML = getDivsElements();
+        flGameOver = false;
+    }
+    seachWordsArrayLength = 0;
+    flGameOver = false;
+    
+    MovElement.innerHTML = `guess ${words[index][0]} `;
+}
 function onChange(event) {
     const wordGuess = event.target.value.toLowerCase();
     event.target.value = '';
-    if (wordGuess.length != N_LETTERS) {
-        alert(`A word should contain ${N_LETTERS} letters`)
-    } else {
-        const wordAr = Array.from(wordGuess);
-        wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
-        const colors = wordAr.map((l, i) => {
-            let index = word.indexOf(l);
-            let res = 'red';
-            if (index > -1) {
-                res = l == word.charAt(i) ? 'green' : 'yellow';
+    if (flGameOver) {
+        alert("game is over");
+        return;
+    }
+    trials++;
+    showTrialsMessage(trials, wordGuess);
+    const wordArray = Array.from(wordGuess);
+    let color = seachWordsArray.map((len) => {
+        return wordArray.includes(len) ? 'white' : 'black';
+    })
+    color.forEach((colorl, index) => {
+        if (colorl === 'white') {
+            if (letterElements[index].style.background !== 'white') {
+                seachWordsArrayLength++;
+                letterElements[index].style.background = colorl;
             }
-            return res;
-        })
-        colors.forEach((c, i) =>
-            letterElements[i].style.color = c)
+        }
+    });
+    if (seachWordsArrayLength === seachWordsArray.length) {
+        endGame(true);
     }
-
-    if (wordGuess === word) {
-        lastResult.textContent = 'Congratulations! You guessed the word!';
-        lastResult.style.backgroundColor = 'green';
-        guessCount=0;
-        return;
-    } else if (guessCount === 0) {
-        lastResult.textContent = '!!!Your attempts to guess are over!!!';
-        lastResult.style.backgroundColor = 'red';
-        guessCount=0;
-        return;
-    } else {
-
-        lastResult.textContent = 'Wrong!';
-        lastResult.style.backgroundColor = 'red';
-        guesses.textContent = (`Do you have any attempts left: ${guessCount-1}`);
-    }
-
-    guessCount--;
-
 }
+function endGame(isSuccess) {
+    if (isSuccess) {
+        gameOverElement.innerHTML = "Congratulations you are winner";
+        gameOverElement.style.color = "green";
+        MovElement.innerHTML = '';
+    }
+    trialsElement.innerHTML = '';
+    wordTrElement.innerHTML = '';
+
+    trialsElement.innerHTML = '';
+    flGameOver = true;
+    index = 0;
+}
+startGame()
