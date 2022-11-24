d5de256d0063e1da35fb916701200546dcc6ef0f
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-11-25 02:50:07 +0300

    HW #19

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
-function getOccurrences(strings) {
-    let strArr = Array.from(strings);
-    const occurrences = {};
-    strArr.forEach(element => {
-        if (occurrences[element]) {
-            occurrences[element]++;
-        } else {
-            occurrences[element] = 1;
-        }
+function createEmployee(id, name, birthYear, salary) {
+    return { id, name, birthYear, salary };
+}
+
+function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary) {
+
+    let arrayNames = ['Jim', 'Jake', 'Jimes', 'Rob', 'Jack', 'Richie', 'Joe'];
+    let minYear = 2000;
+    let maxYear = 1960;
+    let newArr = [];
+    let birthYearForAverage = 0;
+    if (idDigits = 4) {
+        id_min = 1000;
+        id_max = 9999;
+    }
+    for (i = 1; i <= nEmployees; i++) {
+
+        let id = randomIntFromInterval(id_min, id_max);
+        let name = randomArrayNames(arrayNames);
+        let idName = id + name;
+        let salaryOut = randomIntFromInterval(minSalary, maxSalary);
+        birthYear = randomIntFromInterval(minYear, maxYear);
+        birthYearForAverage += (2022 - birthYear);
+        console.log(createEmployee(id, idName, birthYear, salaryOut));
+        newArr.push(createEmployee(id, idName, birthYear, salaryOut));
+
+
+    }
+    let averageAge = birthYearForAverage / nEmployees;
+    console.log("Pure mathematics: Average age :" + averageAge);
+    console.log(newArr);
+    console.log("Second Way: Average age :" + getAverageAge(newArr));
+    console.log(getEmployeesBySalary(newArr));
+    increaseSalary(newArr, 8000, 10);
+}
+
+createRandomEmployees(5, 4, 6000, 20000);
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomArrayNames(array) {
+    return array[Math.floor(Math.random() * array.length)] + "";
+}
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + (2022 - user.birthYear), 0) / users.length;
+}
+function getEmployeesBySalary(array) {
+    let salaryFrom = Math.min(array.salary);
+    let salaryTo = Math.max(array.salary);
+
+    return array.sort(function (salaryFrom, salaryTo) { return salaryFrom.salary - salaryTo.salary });
+}
+
+function increaseSalary(array, borderSalary, percent) {
+    let minSalary = array.filter(function (min) {
+        return min.salary < borderSalary;
+    }).map(function (min) {
+        return min.salary + (min.salary * 10 / 100);
     });
-    return occurrences;
+    console.log(minSalary);
+
 }
-
-function isAnagram(str1, str2) {
-    if (str1.length != str2.length) {
-        return false;
-    }
-    let occurrences = getOccurrences(str1.toLowerCase());
-    let arrayFromSTR2 = Array.from(str2.toLowerCase());
-    for (let i = 0; i < arrayFromSTR2.length; i++) {
-        if (occurrences[arrayFromSTR2[i]] == undefined) {
-            return false;
-        }
-        if (--occurrences[arrayFromSTR2[i]] < 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-let str1 = 'yellow';
-console.log(`yellow - weloly ${isAnagram(str1, 'weloly')}`);
-console.log(`yellow - leloyw ${isAnagram(str1, 'leloyw')}`);
-console.log(`yellow - wolley ${isAnagram(str1, 'wolley')}`);
-console.log(`yellow - weloyl ${isAnagram(str1, 'weloyl')}`);
-
-console.log(`yellow - weloll ${isAnagram(str1, 'weloll')}`);
-console.log(`yellow - leloy ${isAnagram(str1, 'leloy')}`);
-console.log(`yellow - wollet ${isAnagram(str1, 'wollet')}`);
-console.log(`yellow - weloyo ${isAnagram(str1, 'weloyo')}`);
-
