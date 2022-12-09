369738582b723e9ecdaa955d75ac5b7923afc80c
Svetlana Doskochinskaia <Light3308@gmail.com>
2022-12-09 13:46:17 +0300

    HW #23

--- src/main.js
+++ src/main.js
@@ -104,11 +104,11 @@ function show(index) {
 function getBookItems(books) {
     return books.map(e =>
         `<li class="books-item"><div class="books-item-container">
-                 <p class="books-item-paragraph">Author: ${e.author} </p>
-                 <p class="books-item-paragraph">Title: ${e.title} </p>
-                 <p class="books-item-paragraph">Genre: ${e.genre}</p>
-                 <p class="books-item-paragraph">Publicationdate: ${e.publicationDate}</p>
-                 <p class="books-item-paragraph">Pages: ${e.pages}</p></li>`).join('');
+                 <p class="books-item">Author: ${e.author} </p>
+                 <p class="books-item">Title: ${e.title} </p>
+                 <p class="books-item">Genre: ${e.genre}</p>
+                 <p class="books-item">Publicationdate: ${e.publicationDate}</p>
+                 <p class="books-item">Pages: ${e.pages}</p></li>`).join('');
 }
 
 window.onSubmit = onSubmit;
